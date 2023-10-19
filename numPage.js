f644e419ac2cb1ed6e3225a4e53678ddf6df19b4
dranoKas <66783863+dranoKas@users.noreply.github.com>
2023-10-19 20:14:47 +0200

    Update numPage.js

--- numPage.js
+++ numPage.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(cors());
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
